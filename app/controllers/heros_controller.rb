ba6660128c8315dfbd1a8ffb457beb5aacaec1db
Alexander Rovang <DXRovang@gmail.com>
2022-01-14 14:20:04 -0500

    adds show

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -3,4 +3,23 @@ class HerosController < ApplicationController
     @Heroes = Hero.all 
     render json: @Heroes
   end
+
+  def show
+    hero = Hero.find_by(id: params[:id])
+    render json: hero
+  end
+
+  private
+
+  def hero_params
+    params.require(:hero).permit(
+      :name,
+      :temperament,
+      :life_span,
+      :origin,
+      :bred_for,
+      :breed_group,
+      :image_url
+    )
+  end
 end
